7744e56485b8cbf25bb404b90c06851a04c7467e
Louis LAZARE <louislazare.pro@gmail.com>
2026-06-30 08:56:57 +0200

    [FEAT] : Add tags

--- src/main/java/net/necrocraft/world/entity/ModEntity.java
+++ src/main/java/net/necrocraft/world/entity/ModEntity.java
@@ -5,6 +5,7 @@ import net.minecraft.world.entity.MobCategory;
 import net.necrocraft.core.NecroCraft;
 import net.necrocraft.world.entity.minion.ZombieMinion;
 import net.neoforged.neoforge.registries.DeferredRegister;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.function.Supplier;
 
@@ -12,7 +13,7 @@ public class ModEntity {
     public static final DeferredRegister.Entities ENTITY_TYPES =
             DeferredRegister.createEntities(NecroCraft.MODID);
 
-    public static final Supplier<EntityType<ZombieMinion>> ZOMBIE_MINION = ENTITY_TYPES.registerEntityType(
+    public static final Supplier<EntityType<@NotNull ZombieMinion>> ZOMBIE_MINION = ENTITY_TYPES.registerEntityType(
             "zombie_minions",
             ZombieMinion::new,
             MobCategory.MISC
